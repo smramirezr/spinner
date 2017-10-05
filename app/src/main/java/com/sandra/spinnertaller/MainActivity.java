acea84298c8f95ac4889b828fdb9fd9ce522068f
sandra <sandramile1979@hotmail.com>
2017-10-05 16:20:28 -0500

    agregue spiner

--- app/src/main/java/com/sandra/spinnertaller/MainActivity.java
+++ app/src/main/java/com/sandra/spinnertaller/MainActivity.java
@@ -4,17 +4,29 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageSwitcher;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.Toast;
+import android.widget.ViewSwitcher;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
     Spinner spinner;
+    private ImageSwitcher imageSwitcher;
+    private int[] galeria = {R.drawable.logo3, R.drawable.images, R.drawable.tec, R.drawable.hombre, R.drawable.mujeres, R.drawable.infantil};
+    private int posicion;
+    private static final int DURACION = 9000;
+    private Timer timer = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,8 +34,41 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         spinner = (Spinner) findViewById(R.id.idspinner);
         spinner();
+        startSlider();
+        imageSwitcher = (ImageSwitcher) findViewById(R.id.imageSwitcher);
+        imageSwitcher.setFactory(new ViewSwitcher.ViewFactory() {
+            public View makeView() {
+                ImageView imageView = new ImageView(MainActivity.this);
+                imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+                return imageView;
+            }
+        });
+
+        Animation fadeIn = AnimationUtils.loadAnimation(this, R.anim.abc_fade_in);
+        Animation fadeOut = AnimationUtils.loadAnimation(this, R.anim.abc_fade_out);
+        imageSwitcher.setAnimation(fadeIn);
+        imageSwitcher.setAnimation(fadeOut);
     }
-    void spinner(){
+
+    void startSlider() {
+        timer = new Timer();
+        timer.scheduleAtFixedRate(new TimerTask() {
+            public void run() {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        imageSwitcher.setImageResource(galeria[posicion]);
+                        posicion++;
+                        if (posicion == galeria.length)
+                            posicion = 0;
+                    }
+                });
+            }
+        }, 0, DURACION);
+    }
+
+
+    void spinner() {
 
         List list = new ArrayList();
 
@@ -34,15 +79,15 @@ public class MainActivity extends AppCompatActivity {
         list.add("Moda Mujer");
         list.add("Infantil");
 
-        ArrayAdapter arrayAdapter=new ArrayAdapter(this, android.R.layout.simple_dropdown_item_1line, list);
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_dropdown_item_1line, list);
         arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
         spinner.setAdapter(arrayAdapter);
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener(){
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
 
             @Override
-            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l){
-                switch (i){
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                switch (i) {
                     case 0:
                         break;
 
@@ -71,15 +116,19 @@ public class MainActivity extends AppCompatActivity {
                         startActivity(inf);
                         break;
                 }
-            }
-            @Override
-            public void onNothingSelected(AdapterView<?> adapterView){
-                Toast.makeText(MainActivity.this, "no hace nada", Toast.LENGTH_SHORT).show();
+
             }
 
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                Toast.makeText(MainActivity.this, "no ha elegido nada", Toast.LENGTH_SHORT).show();
+            }
         });
+
+        }
     }
-}
+
+
 
 
 
